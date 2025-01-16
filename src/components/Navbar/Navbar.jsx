c8c5f05addcdc9fbdc40f325e669de3fcb025484
Mujahid <eng.mdmuzahidulislam@gmail.com>
2025-01-16 12:39:17 +0600

    figma and requirement docs added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,8 +8,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><NavLink to={'/'} className={({ isActive }) => isActive ? "primary-border text-[#23BE0A]" : ""
-                        }>Home</NavLink></li>
+                        <li><NavLink to={'/'} className={({ isActive }) => isActive && "primary-border text-[#23BE0A]"}>Home</NavLink></li>
                         <li><NavLink to={'/bookList'} className={({ isActive }) => isActive ? "primary-border text-[#23BE0A]" : ""
                         }>Listed Books</NavLink></li>
                         <li><NavLink to={'/statistics'} className={({ isActive }) => isActive ? "primary-border text-[#23BE0A]" : ""
